bfb1aa9ad0dd1b7e85c46be459e13fbd0b96dc0a
comment <obrazovsergey@gmail.com>
2024-02-06 22:56:04 +0300

    asdas

--- src/services/FileUploadService.ts
+++ src/services/FileUploadService.ts
@@ -2,19 +2,20 @@ import http from "../http-common";
 
 const upload = (file: File, onUploadProgress: (progressEvent: any) => void): Promise<any> => {
   let formData = new FormData();
+  console.log(formData);
+  formData.append("image[]", file);
 
-  formData.append("file", file);
-
-  return http.post("/upload", formData, {
+  return http.post("http://127.0.0.1:8000/api/v1/items/3/uploadImage", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
+      'Authorization': 'Bearer ' + '3STcsV0e5DvWC1jOMu16BU7r6xShhCxW0lXLqani56b11526'
     },
     onUploadProgress,
   });
 };
 
 const getFiles = () : Promise<any> => {
-  return http.get("/files");
+  return http.get("http://127.0.0.1:8000/storage/images");
 };
 
 const FileUploadService = {
